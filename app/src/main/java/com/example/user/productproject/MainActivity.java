c67f325abf92cb6ac76bd05c8ed70dcb93a1d377
XXuanTW <30396994+XXuanTW@users.noreply.github.com>
2018-04-23 16:00:36 +0800

    Xuan_0423_1
    New
    Layout_Ok

---
+++ app/src/main/java/com/example/user/productproject/MainActivity.java
@@ -0,0 +1,259 @@
+package com.example.user.productproject;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.os.AsyncTask;
+import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textBarCode;
+    TextView textWareHouse;
+    TextView textProduct;
+    TextView textCheckTime;
+    Toolbar toolbar;
+    NavigationView liftmenu;
+    DrawerLayout mainlayout;
+    CameraSource cameraSource;
+    SurfaceView CamreaView;
+    BarcodeDetector barcodeDetector;
+    Button CheckidButton;
+    final int CameraID = 1;
+    String url;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textWareHouse = (TextView)findViewById(R.id.textWarehouse);
+        textProduct = (TextView)findViewById(R.id.textProduct);
+        textBarCode = (TextView)findViewById(R.id.textBarCode);
+        textCheckTime = (TextView)findViewById(R.id.textCheckTime);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        liftmenu = (NavigationView)findViewById(R.id.liftmenu);
+        mainlayout = (DrawerLayout)findViewById(R.id.main_layout);
+        CamreaView = (SurfaceView) findViewById(R.id.CamareView);
+        CheckidButton = (Button)findViewById(R.id.CheckButton);
+        SetToolBar();
+        SetLifeMenu();
+
+        //QRcode掃描配置
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+        //照相機配置
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .setAutoFocusEnabled(true)
+                .build();
+        //SurfaceView配置
+        CamreaView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA}, CameraID);
+                    return;
+                }
+                try {
+                    cameraSource.start(CamreaView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
+                if (qrcodes.size() != 0)
+                    textBarCode.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                            vibrator.vibrate(500);
+
+                            textBarCode.setText(qrcodes.valueAt(0).displayValue);
+                            runCode();
+
+                        }
+
+                        private void runCode() {
+                            url ="";
+                            url =  url+textBarCode.getText().toString();
+                            new TransTask().execute(url);
+                        }
+                    });
+            }
+        });
+
+    }
+
+    //Json 解析
+    class TransTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... params) {
+            StringBuilder sb = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                String line = in.readLine();
+                while (line != null) {
+                    Log.d("HTTP", line);
+                    sb.append(line);
+                    line = in.readLine();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return sb.toString();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("JSON", s);
+            parseJSON(s);
+        }
+
+        private void parseJSON(String s) {
+            ArrayList<Json> trans = new ArrayList<>();
+            try {
+                JSONArray array = new JSONArray(s);
+                for (int i=0; i<array.length(); i++){
+                    JSONObject obj = array.getJSONObject(i);
+//                    int id = obj.getInt("id");
+                    Json t = new Json();
+
+                    trans.add(t);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    //ToolBar
+    private void SetToolBar() {
+
+        toolbar.setTitle("檢票系統");
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);//toolbar 上面
+
+
+        ActionBarDrawerToggle mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mainlayout, toolbar, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mActionBarDrawerToggle.syncState();
+    }
+
+    //左側表單
+    private void SetLifeMenu() {
+        liftmenu.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // 點選時收起選單
+                mainlayout.closeDrawer(GravityCompat.START);
+                // 取得選項id
+                int id = item.getItemId();
+                // 依照id判斷點了哪個項目並做相應事件
+                if (id == R.id.action_camera) {
+                    Toast.makeText(MainActivity.this, "目前顯示頁面：盤點系統", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+    }
+
+    //權限
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CameraID: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(CamreaView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+}
